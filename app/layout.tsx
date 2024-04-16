9c45be2dbe118a453d11cf5e726b2d897fbba0e4
HenrikGevorgyan <henrik.gevorgyan@outlook.com>
2024-04-17 02:57:52 +0400

    add favicon.ico

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from 'next/font/google'
 
 import Header from '@/components/ui/header'
 import Banner from '@/components/banner'
+import Head from "next/head";
 
 const inter = Inter({
   subsets: ['latin'],
@@ -26,6 +27,9 @@ export default function RootLayout({
       <meta property="og:title" content={metadata.title} data-rh="true"></meta>
       <meta property="og:description" content={metadata.description} data-rh="true"></meta>
       <meta property="og:image" content="/images/banner1.jpg" data-rh="true"></meta>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <body className={`${inter.variable} font-inter antialiased bg-white text-gray-900 tracking-tight`}>
         <div className="flex flex-col min-h-screen overflow-hidden supports-[overflow:clip]:overflow-clip">
           <Header />
