5b2f8eca792e87a45e8761f814ad6ddc6c38f017
HenrikGevorgyan <henrik.gevorgyan@outlook.com>
2024-04-26 02:48:02 +0400

    change grid for partners

--- components/testimonials.tsx
+++ components/testimonials.tsx
@@ -34,7 +34,7 @@ export default function Testimonials() {
           </div>
 
           {/* Items */}
-          <div className="max-w-sm md:max-w-4xl mx-auto grid gap-2 grid-cols-5 md:grid-cols-5">
+          <div className="max-w-sm md:max-w-4xl mx-auto grid gap-2 lg:grid-cols-5 sm:grid-cols-2">
 
             {/* Item */}
             <div className="flex items-center justify-center py-2 col-span-2 md:col-auto">
